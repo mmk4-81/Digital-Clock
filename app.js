e965f2b18fc12a47bc3db2ecc65fb4e11cd52a78
mmk <mmkkarbalaei@gmail.com>
2024-02-22 00:42:27 +0330

    setInterval

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 let hrs = document.getElementById('hrs');
 let mins = document.getElementById('mts');
 let sec = document.getElementById('sc');
+
+
+setInterval(() => {
+    let currentTime = new Date();
+    hrs.innerHTML = currentTime.getHours().toString().padStart(2, '0');
+    mins.innerHTML = currentTime.getMinutes().toString().padStart(2, '0');
+    sec.innerHTML = currentTime.getSeconds().toString().padStart(2, '0');
+}, 1000);
